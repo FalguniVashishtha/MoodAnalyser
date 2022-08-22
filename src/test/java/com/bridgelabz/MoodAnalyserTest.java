3f2a29e868f4f9f3e1b12a50313040f60ca3ce31
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-22 17:42:00 +0530

    Repeat TC_1.1 added

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest
 {
     @Test
-    public void caseSad(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String result = moodAnalyser.analyseMood("I am In Sad mood.");
+    public void caseSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In Sad mood.");
+        String result = moodAnalyser.analyseMood();
         System.out.println(result);
-        Assertions.assertEquals("Sad" , result);
+        Assertions.assertEquals("SAD", result);
     }
 
 }
