988e9c7bd5ddb6da11b5605c9bd86361860765a4
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-22 18:24:21 +0530

    Handle exception for Invalid mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,21 +2,25 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
     public static String message;
-    //constructor
-    public MoodAnalyser(String message)
-    {
+
+    public MoodAnalyser(String message) {
         this.message = message;
         analyseMood();
     }
-    public String analyseMood()
-    {
-        if (message.contains("Sad"))
-            return "SAD";
-        else if (message.contains("Happy"))
-            return "HAPPY";
-        else
-            return "HAPPY";
 
+    public String analyseMood() {
+        try{
+            if (message.contains("Sad"))
+                return "SAD";
+            else if (message.contains("Happy"))
+                return "HAPPY";
+
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Please Enter the Valid Mood.");
+        }
+        return "";
     }
 
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,11 +5,25 @@ import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest
 {
     @Test
-        public void caseHappy(){
+    public void caseSad(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In Sad mood.");
+        String result = moodAnalyser.analyseMood();
+        System.out.println(result);
+        Assertions.assertEquals("SAD" , result);
+    }
+    @Test
+    public void caseHappy(){
         MoodAnalyser moodAnalyser = new MoodAnalyser("I Am In a Happy mood.");
         String result = moodAnalyser.analyseMood();
         System.out.println(result);
         Assertions.assertEquals("HAPPY" , result);
+    }
+    @Test
+    public void caseNullInvalid(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String result = moodAnalyser.analyseMood();
+        System.out.println(result);
+        Assertions.assertEquals("InvalidMood" , result);
     }
 
 }
